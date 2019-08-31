eede9780f5d9c3fd412fe4070d568fa0bb98cdf7
Hiroshi Matsuoka <pipomaomy2005@gmail.com>
2019-08-31 15:05:38 +0900

    Update for 10.0.16299.0

--- VideoEffectLibrary/Class1.cs
+++ VideoEffectLibrary/Class1.cs
@@ -3,10 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Foundation.Collections;
+using Windows.Graphics.DirectX.Direct3D11;
+using Windows.Media;
+using Windows.Media.Effects;
+using Windows.Media.MediaProperties;
 
 namespace VideoEffectLibrary
 {
-    public sealed class ExampleVideoEffect
+    public sealed class ExampleVideoEffect: IBasicVideoEffect
     {
+        public void SetEncodingProperties(VideoEncodingProperties encodingProperties, IDirect3DDevice device)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ProcessFrame(ProcessVideoFrameContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Close(MediaEffectClosedReason reason)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DiscardQueuedFrames()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsReadOnly => throw new NotImplementedException();
+
+        public IReadOnlyList<VideoEncodingProperties> SupportedEncodingProperties => throw new NotImplementedException();
+
+        public MediaMemoryTypes SupportedMemoryTypes => throw new NotImplementedException();
+
+        public bool TimeIndependent => throw new NotImplementedException();
+
+        public void SetProperties(IPropertySet configuration)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
