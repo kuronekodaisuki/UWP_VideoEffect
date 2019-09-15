b6aa8fab10c10bfd3b101210f1c275552721e99e
Hiro Matsuoka <pipomaomy2005@gmail.com>
2019-09-15 22:24:45 +0900

    Add new project

--- VideoEffectLibrary/VideoEffect.cs
+++ VideoEffectLibrary/VideoEffect.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using Windows.Foundation.Collections;
+using Windows.Graphics.Imaging;
 using Windows.Graphics.DirectX.Direct3D11;
 using Windows.Media;
 using Windows.Media.Effects;
@@ -16,37 +18,45 @@ namespace VideoEffectLibrary
 {
     public sealed class ExampleVideoEffect: IBasicVideoEffect
     {
-        public void SetEncodingProperties(VideoEncodingProperties encodingProperties, IDirect3DDevice device)
-        {
-            throw new NotImplementedException();
-        }
+        private IPropertySet _properties;
+        private IDirect3DDevice _device;
 
         public void ProcessFrame(ProcessVideoFrameContext context)
         {
             throw new NotImplementedException();
         }
 
+
+        public void SetEncodingProperties(VideoEncodingProperties encodingProperties, IDirect3DDevice device)
+        {
+            _device = device;
+        }
+
         public void Close(MediaEffectClosedReason reason)
         {
-            throw new NotImplementedException();
         }
 
         public void DiscardQueuedFrames()
         {
-            throw new NotImplementedException();
         }
 
-        public bool IsReadOnly => throw new NotImplementedException();
+        public bool IsReadOnly
+        {
+            get { return true; }
+        }
 
         public IReadOnlyList<VideoEncodingProperties> SupportedEncodingProperties => throw new NotImplementedException();
 
-        public MediaMemoryTypes SupportedMemoryTypes => throw new NotImplementedException();
-
-        public bool TimeIndependent => throw new NotImplementedException();
+        public bool TimeIndependent
+        {
+            get { return true; }
+        }
 
         public void SetProperties(IPropertySet configuration)
         {
-            throw new NotImplementedException();
+            _properties = configuration;
         }
+
+        MediaMemoryTypes IBasicVideoEffect.SupportedMemoryTypes => throw new NotImplementedException();
     }
 }
